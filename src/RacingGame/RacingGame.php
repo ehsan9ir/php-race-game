fefc4c3af8b927bc9fd5af854769f31533781ef4
Ehsan9 <h.ehsan24@gmail.com>
2024-03-03 09:24:56 +0330

    feat(RacingGame): Implement startRace and determineWinner Function, enhancing the racing game output for a more engaging experience

--- src/RacingGame/RacingGame.php
+++ src/RacingGame/RacingGame.php
@@ -38,4 +38,42 @@ class RacingGame
         // Calculate time(hour) based on converted maximum speed
         return $this->distance / $maxSpeedInKmPerHour;
     }
+
+    public function startRace(): string
+    {
+        $this->printHeader();
+
+        $timePlayer1 = $this->calculateTime($this->player1);
+        $timePlayer2 = $this->calculateTime($this->player2);
+
+        $this->printRaceResult($timePlayer1, $timePlayer2);
+
+        $this->winner = $this->determineWinner($timePlayer1, $timePlayer2);
+
+        return $this->getWinnerText($this->winner);
+    }
+
+    private function determineWinner(float $timePlayer1, float $timePlayer2): Player
+    {
+        return $timePlayer1 < $timePlayer2 ? $this->player1 : $this->player2;
+    }
+
+    private function printHeader(): void
+    {
+        echo "\n🚩🚩 **************************** 🚩🚩\n";
+        echo "Race started!\n";
+    }
+
+    private function printRaceResult(float $timePlayer1, float $timePlayer2): void
+    {
+        echo "\n🏁🏁 Race Results: 🏁🏁\n";
+        echo "{$this->player1->getName()}'s time: $timePlayer1 hours\n";
+        echo "{$this->player2->getName()}'s time: $timePlayer2 hours\n";
+        echo "\nRace distance: $this->distance kilometers\n";
+    }
+
+    private function getWinnerText(Player $winner): string
+    {
+        return "\n🏆 {$winner->getName()} ({$winner->getSelectedVehicle()->getName()}) wins the race!\n";
+    }
 }
